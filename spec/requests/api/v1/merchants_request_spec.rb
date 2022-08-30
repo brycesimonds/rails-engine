b16fde5e9a54fd68df2592ed51d1e5e7483ec898
Bryce Simonds <bsimonds14@gmail.com>
2022-08-29 18:47:43 -0600

    Feat: can get one merchant by its id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,18 +22,18 @@ RSpec.describe "Merchants API" do
   end
 
   it 'can get one merchant by its id' do
-    id = create(:merchant).id 
+    id = create(:merchant).id
 
     get "/api/v1/merchants/#{id}"
-    binding.pry
+
     merchant = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
 
-    expect(merchant).to have_key(:id)
-    expect(merchant[:id]).to eq(id)
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id].to_i).to eq(id)
 
-    expect(merchant).to have_key(:name)
-    expect(merchant[:name]).to be_a(String)
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
