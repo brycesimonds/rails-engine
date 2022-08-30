65ea0b0889f6f48fa0950a468bee4cd7b162b27f
Bryce Simonds <bsimonds14@gmail.com>
2022-08-30 17:33:56 -0600

    Tests: happy and sad path testing for getting one item by its id

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -30,4 +30,34 @@ RSpec.describe "Items API" do
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
     end
   end
+
+  it 'can get one item by its id' do
+    item_1 = create(:item)
+    item_2 = create(:item)
+
+
+    get "/api/v1/items/#{item_1.id}"
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+
+    expect(response).to be_successful
+
+    expect(item).to have_key(:id)
+    expect(item[:id].to_i).to eq(item_1.id)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+  end
+
+  it 'returns 404 if no such item id exists' do
+    item_1 = create(:item)
+    item_2 = create(:item)
+
+    get "/api/v1/items/567"
+    response_body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.status).to eq(404)
+    expect(response_body[:message]).to eq("Couldn't find Item with 'id'=567")
+  end
 end 
