3cd17b8878c814b634b42e36d4ff634253295f46
Bryce Simonds <bsimonds14@gmail.com>
2022-08-30 11:26:07 -0600

    Feat: validations and relationships are complete

---
+++ app/models/invoice.rb
@@ -0,0 +1,11 @@
+class Invoice < ApplicationRecord
+  validates_presence_of :status
+
+  belongs_to :customer
+  belongs_to :merchant
+  has_many :transactions
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
+
+  enum status: { "in progress" => 0, "cancelled" => 1, "completed" => 2 }
+end 
