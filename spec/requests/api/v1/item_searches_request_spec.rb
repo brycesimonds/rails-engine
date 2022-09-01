4325ab9b67ce0e9db53ebe2b5456bc4ed1274a23
Bryce Simonds <bsimonds14@gmail.com>
2022-09-01 14:41:52 -0600

    Test: sends an item who matches min and max search params

--- spec/requests/api/v1/item_searches_request_spec.rb
+++ spec/requests/api/v1/item_searches_request_spec.rb
@@ -117,4 +117,37 @@ RSpec.describe "Item Searches API" do
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
+
+  it "sends an item who matches the min and max price parameters searched for" do
+    merchant = Merchant.create!(name: "Harold the Big")
+
+    item_1 = Item.create!(name: "Cherry", description: Faker::Beer.style, unit_price: 10.00, merchant_id: merchant.id)
+    item_2 = Item.create!(name: "Apple", description: Faker::Beer.style, unit_price: 49.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "Pear", description: Faker::Beer.style, unit_price: 50.00, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Berry", description: Faker::Beer.style, unit_price: 50.01, merchant_id: merchant.id)
+
+    get '/api/v1/items/find?max_price=150&min_price=50'
+   
+    expect(response).to be_successful
+  
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+ 
+    expect(item[:attributes][:name]).to eq("Berry")
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:attributes]).to have_key(:merchant_id)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
+  end
 end 
