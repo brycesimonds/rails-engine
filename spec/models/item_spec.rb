28415f0101cc60802b7694ccc00f8bd86a1e9388
Bryce Simonds <bsimonds14@gmail.com>
2022-09-01 11:56:22 -0600

    Test: search class method for an item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe Item do
     it { should have_many(:transactions).through(:invoices) }
     it { should have_many(:customers).through(:invoices) }
   end
+
+  describe 'class methods' do 
+    it 'can perform a case insensitive name search and returns first alphabetical item' do
+      merchant = Merchant.create!(name: "Harold the Big")
+
+      item_1 = Item.create!(name: "Peach", description: Faker::Beer.style, unit_price: Faker::Number.decimal(l_digits: 2), merchant_id: merchant.id)
+      item_2 = Item.create!(name: "Pineapple", description: Faker::Beer.style, unit_price: Faker::Number.decimal(l_digits: 2), merchant_id: merchant.id)
+      item_3 = Item.create!(name: "Pear", description: Faker::Beer.style, unit_price: Faker::Number.decimal(l_digits: 2), merchant_id: merchant.id)
+      item_4 = Item.create!(name: "Berry", description: Faker::Beer.style, unit_price: Faker::Number.decimal(l_digits: 2), merchant_id: merchant.id)
+
+      expect(Item.search("p")).to eq([item_1, item_2, item_3])
+      expect(Item.search("P")).to eq([item_1, item_2, item_3])
+      expect(Item.search("bEr")).to eq([item_4])
+    end
+  end
 end 
