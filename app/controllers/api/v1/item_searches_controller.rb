3005b363ab188ae39df0d281ce5451c8ddd0e6fc
Bryce Simonds <bsimonds14@gmail.com>
2022-09-01 18:25:28 -0600

    All tests in rspec and postman passing

--- app/controllers/api/v1/item_searches_controller.rb
+++ app/controllers/api/v1/item_searches_controller.rb
@@ -1,13 +1,32 @@
 class Api::V1::ItemSearchesController < ApplicationController
   def index
-    if params[:name]
-      json_response(ItemSerializer.new(Item.search_name(params[:name])))
+    if params[:min_price].to_i < 0 || params[:max_price].to_i < 0
+      json_response({ error: 'Query not permitted' }, :bad_request)
+    elsif params[:min_price].to_i < 0 || params[:max_price].to_i < 0
+      render status: 400
+    elsif params[:min_price] && params[:name]
+      render status: 400
+    elsif params[:max_price] && params[:name]
+      render status: 400
+    elsif params[:min_price] && params[:max_price] && params[:name]
+      render status: 400
     elsif params[:min_price] && params[:max_price]
       json_response(ItemSerializer.new(Item.search_min_max_price(params[:min_price], params[:max_price])))
     elsif params[:min_price]
-      json_response(ItemSerializer.new(Item.search_min_price(params[:min_price])))
+      if Item.search_min_price(params[:min_price]).nil?
+        render json: {data: { } }
+      else
+        json_response(ItemSerializer.new(Item.search_min_price(params[:min_price])))
+      end 
     elsif params[:max_price]
       json_response(ItemSerializer.new(Item.search_max_price(params[:max_price])))
+    elsif params[:name]
+      if Item.search_name(params[:name]).nil?
+        render json: {data: { } }
+      else
+        json_response(ItemSerializer.new(Item.search_name(params[:name])))
+      end
     end
   end
 end
+
--- spec/requests/api/v1/item_searches_request_spec.rb
+++ spec/requests/api/v1/item_searches_request_spec.rb
@@ -48,8 +48,8 @@ RSpec.describe "Item Searches API" do
   
     response_body = JSON.parse(response.body, symbolize_names: true)
     item = response_body[:data]
- 
-    expect(item).to be(nil)
+
+    expect(item).to eq({})
   end
 
   it "sends an item who matches the min price parameters searched for" do
@@ -66,7 +66,7 @@ RSpec.describe "Item Searches API" do
   
     response_body = JSON.parse(response.body, symbolize_names: true)
     item = response_body[:data]
- 
+    
     expect(item[:attributes][:name]).to eq("Berry")
 
     expect(item).to have_key(:id)
@@ -149,5 +149,49 @@ RSpec.describe "Item Searches API" do
 
     expect(item[:attributes]).to have_key(:merchant_id)
     expect(item[:attributes][:merchant_id]).to be_an(Integer)
+  end
+
+  it "returns a 400 status if min price is less than 0" do
+    merchant = Merchant.create!(name: "Harold the Big")
+
+    item_1 = Item.create!(name: "Cherry", description: Faker::Beer.style, unit_price: 10.00, merchant_id: merchant.id)
+    item_2 = Item.create!(name: "Apple", description: Faker::Beer.style, unit_price: 49.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "Pear", description: Faker::Beer.style, unit_price: 50.00, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Berry", description: Faker::Beer.style, unit_price: 50.01, merchant_id: merchant.id)
+
+    get '/api/v1/items/find?min_price=-12'
+
+    expect(response.status).to eq(400)
+  end 
+
+  it "returns a 400 status if name is sent with min price" do
+    merchant = Merchant.create!(name: "Harold the Big")
+
+    item_1 = Item.create!(name: "Cherry", description: Faker::Beer.style, unit_price: 10.00, merchant_id: merchant.id)
+    item_2 = Item.create!(name: "Apple", description: Faker::Beer.style, unit_price: 49.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "Pear", description: Faker::Beer.style, unit_price: 50.00, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Berry", description: Faker::Beer.style, unit_price: 50.01, merchant_id: merchant.id)
+
+    get '/api/v1/items/find?name=berry&min_price=50'
+   
+    expect(response.status).to eq(400)
+  end 
+
+  it "errors if no item  matches the min price parameters searched for" do
+    merchant = Merchant.create!(name: "Harold the Big")
+
+    item_1 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 10.00, merchant_id: merchant.id)
+    item_2 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 49.99, merchant_id: merchant.id)
+    item_3 = Item.create!(name: "Pear", description: Faker::Beer.style, unit_price: 50.00, merchant_id: merchant.id)
+    item_4 = Item.create!(name: "Berry", description: Faker::Beer.style, unit_price: 50.01, merchant_id: merchant.id)
+
+    get '/api/v1/items/find?min_price=999999999'
+   
+    expect(response).to be_successful
+  
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    item = response_body[:data]
+   
+    expect(item).to eq({})
   end
 end 
